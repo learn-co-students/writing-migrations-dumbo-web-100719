4709b1592bf61ac6644c2b362baa70d5e083bfb2
AlinaArakelyan <alinaarakelyan90@gmail.com>
2019-10-21 22:49:43 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,10 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class CreateStudents < ActiveRecord::Migration[5.2r]
+
+    def change
+        create_table :students do |t|
+            t.string :name
+        end
+    end 
+
+
 end
